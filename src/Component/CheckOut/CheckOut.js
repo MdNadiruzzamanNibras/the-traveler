fd79a12824c8f041c491bed8b65e6303d51c09de
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-18 20:07:12 +0600

    checkOut complete

--- src/Component/CheckOut/CheckOut.js
+++ src/Component/CheckOut/CheckOut.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './CheckOut.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const CheckOut = () => {
+    const hanleCheckOut=()=>{
+        toast('Thank you for the booking.');
+    }
     return (
         <div className='checkout-container'>
             <form >
@@ -8,8 +13,8 @@ const CheckOut = () => {
             <input type="email" name="email" id="" placeholder='Your Name' required/>
             <input type="text" name="address" id="" placeholder='Your Address' required/>
             <input type="number" name="phone" id="" placeholder='Your Phone number' required/>
-
-
+             <button onClick={hanleCheckOut} type='submit'>Check Out</button>
+             <ToastContainer/>
             </form>
         </div>
     );
