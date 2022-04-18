f32d3905162e63ebf63f6356edb4aaac16e55328
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-04-18 22:51:59 +0600

    last touch

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -3,12 +3,14 @@ import myPic from '../../images/my pic/nibras.jpg'
 
 const About = () => {
     return (
-        <div>
+        <div style={{ margin:'30px auto', textAlign:'center'}}>
+           
             <img src={myPic} alt="" />
             <h1>Goal as a web developer</h1>
             <p>My first goal is this year well known about web developting related 
                 work. Second goal create a little position in the developer world. Next four years build to myself as an established developer.This is my chaallengae in sha Allah I can do it. </p>
-        </div>
+            </div>
+   
     );
 };
 
